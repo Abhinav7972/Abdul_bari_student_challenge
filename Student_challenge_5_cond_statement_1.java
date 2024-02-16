6be03ff62726ca82c94cab8af459e0846263bc7c
abhinav_soni <abhinavsoni316@gmail.com>
2024-02-16 21:46:28 +0530

    even odd condition statement

---
+++ Student_challenge_5_cond_statement_1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Student_challenge_5_cond_statement_1
+{
+public static void main(String [] args)
+{
+Scanner sc = new Scanner(System.in);	
+//even odd number	
+System.out.println("enter number");
+int n =sc.nextInt();
+if(n%2==0)
+{
+	System.out.println("number is even");
+}
+else
+{
+	
+System.out.println("number is odd");	
+}
+}
+}
