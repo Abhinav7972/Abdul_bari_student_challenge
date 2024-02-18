d4a3e90b270f9b5eb33e0b3e8051615623a67216
abhinav_soni <abhinavsoni316@gmail.com>
2024-02-18 14:03:52 +0530

    day and number using switch

---
+++ Student_challenge_8_day_and_number.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+class Student_challenge_8_day_and_number
+{
+public static void main(String [] args)
+{
+Scanner s = new Scanner(System.in);
+System.out.println("Enter day number");
+int day = s.nextInt();
+
+switch(day)
+{
+	case 1: System.out.println("monday");
+	break;
+	
+	case 2: System.out.println("tuesday");
+	break;
+	
+	case 3: System.out.println("wednesday");
+	break;
+	
+	case 4: System.out.println("thursday");
+	break;
+	
+	case 5: System.out.println("friday");
+	break;
+	
+	case 6: System.out.println("saturday");
+	break;
+	
+	case 7: System.out.println("sunday");
+	break;
+	
+	default : System.out.println("invalid number");
+}
+	
+}
+}
