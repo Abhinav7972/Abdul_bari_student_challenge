5aea2acd8ad9c0753829434d99135df0d0c9b3d6
abhinav_soni <abhinavsoni316@gmail.com>
2024-02-24 14:05:22 +0530

    adding elements of array

---
+++ Student_challenge_17_array_practice_1.java
@@ -0,0 +1,17 @@
+class Student_challenge_17_array_practice_1
+{
+public static void main(String [] args)
+{
+int sum =0;
+int a[] = {1,2,3,4};
+
+//sum of array
+for(int i =0; i <a.length;i++)
+{
+	sum = sum +a[i];
+}
+
+System.out.println("sum of array is :" + sum);
+
+}
+}
