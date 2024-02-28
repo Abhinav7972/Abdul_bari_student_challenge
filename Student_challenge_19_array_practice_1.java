485fa1c211f745d89024f6e0f203013fa551c69f
abhinav_soni <abhinavsoni316@gmail.com>
2024-02-28 13:58:23 +0530

    copying an array

---
+++ Student_challenge_19_array_practice_1.java
@@ -0,0 +1,27 @@
+class Student_challenge_19_array_practice_1
+{
+public static void main(String [] args)
+{
+int a[] = {1,2,3,4,5,6};
+int b[] = new int [6];
+
+
+for(int i = 0 ; i<a.length;i++)
+{
+b[i] = a[i]; 
+
+ 
+}
+
+
+
+for (int x:b)
+{
+	System.out.print(x);
+}	
+
+
+
+}
+
+}
