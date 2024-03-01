391741d32135adf1bd3f7fcf3be300a4ea3fa88e
abhinav_soni <abhinavsoni316@gmail.com>
2024-03-01 23:07:42 +0530

    sorting array of string

---
+++ Student_challenge_20_array_3.java
@@ -0,0 +1,27 @@
+class Student_challenge_20_array_3
+{
+
+public static void main(String [] args)
+{
+
+String arr[]= {"java","python","pascal","smalltalk","ada","basic"};
+java.util.Arrays.sort(arr);
+
+for (String i : arr)
+System.out.println(i + "");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
